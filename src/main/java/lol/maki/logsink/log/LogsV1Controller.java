d806c5379ccef773fdcaced420078f0aab54b1ce
Toshiaki Maki <makingx@gmail.com>
2024-05-24 01:48:23 +0900

    Rename

--- src/main/java/lol/maki/logsink/log/LogsV1Controller.java
+++ src/main/java/lol/maki/logsink/log/LogsV1Controller.java
@@ -14,9 +14,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LogSinkController {
+public class LogsV1Controller {
 
-	private final Logger log = LoggerFactory.getLogger(LogSinkController.class);
+	private final Logger log = LoggerFactory.getLogger(LogsV1Controller.class);
 
 	@PostMapping(path = "/v1/logs", consumes = MediaType.APPLICATION_PROTOBUF_VALUE)
 	public void logs(@RequestBody LogsData logs) throws InvalidProtocolBufferException {
