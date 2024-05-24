9ab5b85fae84fbe8b917c4b4368dbdb321a22982
Toshiaki Maki <makingx@gmail.com>
2024-05-24 16:56:34 +0900

    Update

--- README.md
+++ README.md
@@ -9,29 +9,30 @@ a tiny OTLP log receiver
 cat src/test/resources/logs.json | curl -H "Content-Type: application/json" -s http://localhost:4318/v1/logs --data-binary @- -v
 ```
 
-You'll see the following log
+You'll see the following log output:
 
 ```
 ResourceLogs #0
-Resource attributes:
-  -> service.name : my.service
-  SpanLogs #0
-  Scope: my.library 1.0.0
-  Scope attributes:
-    -> my.scope.attribute : some scope attribute
-    Log #0
-      Timestamp: 2018-12-13T14:51:00.300Z
-      Severity: Information
-      Body:  Example log record
-      Trace ID: e41f0414517bf7cd37f35d370f6ebd07adf7f35dc50bad02
-      Span ID:  104135f41ec40b70b5075ef8
-      Attributes:
-      -> string.attribute : some string
-      -> boolean.attribute : Bool(true)
-      -> int.attribute : Int(10)
-      -> double.attribute : Double(637.704)
-      -> array.attribute : Array([many, values])
-      -> map.attribute : KvList({some.map.key=some value})
+Attributes:
+	-> service.name	: "my.service"
+	SpanLogs #0
+	Scope: my.library	1.0.0
+	Attributes:
+		-> my.scope.attribute	: "some scope attribute"
+		Log #0
+			Timestamp	: 2018-12-13T14:51:00.300Z
+			Severity	: Information
+			Body    	: "Example log record"
+			Trace ID	: e41f0414517bf7cd37f35d370f6ebd07adf7f35dc50bad02
+			Span ID 	: 104135f41ec40b70b5075ef8
+			Flags   	: 0
+			Attributes	: 
+				-> string.attribute	: "some string"
+				-> boolean.attribute	: true
+				-> int.attribute	: 10
+				-> double.attribute	: 637.704
+				-> array.attribute	: ["many", "values"]
+				-> map.attribute	: {some.map.key="some value"}
 ```
 
 ## How to rename the project
--- src/main/java/lol/maki/logsink/logs/LogsV1Controller.java
+++ src/main/java/lol/maki/logsink/logs/LogsV1Controller.java
@@ -31,50 +31,54 @@ public class LogsV1Controller {
 			ResourceLogs resourceLogs = logs.getResourceLogs(i);
 			message.append("ResourceLogs #").append(i).append(System.lineSeparator());
 			Resource resource = resourceLogs.getResource();
-			message.append("Resource attributes:").append(System.lineSeparator());
+			message.append("Attributes:").append(System.lineSeparator());
 			resource.getAttributesList()
-				.forEach(attribute -> message.append("  -> ")
+				.forEach(attribute -> message.append("\t-> ")
 					.append(attribute.getKey())
-					.append(" : ")
+					.append("\t: ")
 					.append(any(attribute.getValue()))
 					.append(System.lineSeparator()));
 			for (int j = 0; j < resourceLogs.getScopeLogsCount(); j++) {
 				ScopeLogs scopeLogs = resourceLogs.getScopeLogs(j);
-				message.append("  SpanLogs #").append(j).append(System.lineSeparator());
+				message.append("\tSpanLogs #").append(j).append(System.lineSeparator());
 				InstrumentationScope scope = scopeLogs.getScope();
-				message.append("  Scope: ")
+				message.append("\tScope: ")
 					.append(scope.getName())
-					.append(" ")
+					.append("\t")
 					.append(scope.getVersion())
 					.append(System.lineSeparator());
-				message.append("  Scope attributes:").append(System.lineSeparator());
+				message.append("\tAttributes:").append(System.lineSeparator());
 				scope.getAttributesList()
-					.forEach(attribute -> message.append("    -> ")
+					.forEach(attribute -> message.append("\t\t-> ")
 						.append(attribute.getKey())
-						.append(" : ")
+						.append("\t: ")
 						.append(any(attribute.getValue()))
 						.append(System.lineSeparator()));
 				for (int k = 0; k < scopeLogs.getLogRecordsCount(); k++) {
 					LogRecord logRecord = scopeLogs.getLogRecords(k);
-					message.append("    Log #").append(k).append(System.lineSeparator());
-					message.append("      Timestamp: ")
-						.append(Instant.ofEpochMilli(logRecord.getTimeUnixNano() / 1_000_000))
+					message.append("\t\tLog #").append(k).append(System.lineSeparator());
+					message.append("\t\t\tTimestamp\t: ")
+						.append(Instant.EPOCH.plusNanos(logRecord.getTimeUnixNano()))
 						.append(System.lineSeparator());
-					message.append("      Severity: ")
+					message.append("\t\t\tSeverity\t: ")
 						.append(logRecord.getSeverityText())
 						.append(System.lineSeparator());
-					message.append("      Body:  ").append(any(logRecord.getBody())).append(System.lineSeparator());
-					message.append("      Trace ID: ")
+					message.append("\t\t\tBody    \t: ")
+						.append(any(logRecord.getBody()))
+						.append(System.lineSeparator());
+					message.append("\t\t\tTrace ID\t: ")
 						.append(HexFormat.of().formatHex(logRecord.getTraceId().toByteArray()))
 						.append(System.lineSeparator());
-					message.append("      Span ID:  ")
+					message.append("\t\t\tSpan ID \t: ")
 						.append(HexFormat.of().formatHex(logRecord.getSpanId().toByteArray()))
 						.append(System.lineSeparator());
-					message.append("      Attributes:").append(System.lineSeparator());
+					message.append("\t\t\tFlags   \t: ").append(logRecord.getFlags()).append(System.lineSeparator());
+					;
+					message.append("\t\t\tAttributes\t: ").append(System.lineSeparator());
 					logRecord.getAttributesList()
-						.forEach(attribute -> message.append("      -> ")
+						.forEach(attribute -> message.append("\t\t\t\t-> ")
 							.append(attribute.getKey())
-							.append(" : ")
+							.append("\t: ")
 							.append(any(attribute.getValue()))
 							.append(System.lineSeparator()));
 				}
@@ -85,26 +89,25 @@ public class LogsV1Controller {
 
 	static String any(AnyValue value) {
 		if (value.hasStringValue()) {
-			return "%s".formatted(value.getStringValue());
+			return "\"%s\"".formatted(value.getStringValue());
 		}
 		if (value.hasBoolValue()) {
-			return "Bool(%s)".formatted(value.getBoolValue());
+			return "%s".formatted(value.getBoolValue());
 		}
 		if (value.hasBytesValue()) {
-			return "Bytes(%s)".formatted(value.getBytesValue());
+			return "%s".formatted(value.getBytesValue());
 		}
 		if (value.hasIntValue()) {
-			return "Int(%d)".formatted(value.getIntValue());
+			return "%d".formatted(value.getIntValue());
 		}
 		if (value.hasDoubleValue()) {
-			return "Double(%s)".formatted(value.getDoubleValue());
+			return "%s".formatted(value.getDoubleValue());
 		}
 		if (value.hasArrayValue()) {
-			return "Array(%s)"
-				.formatted(value.getArrayValue().getValuesList().stream().map(LogsV1Controller::any).toList());
+			return "%s".formatted(value.getArrayValue().getValuesList().stream().map(LogsV1Controller::any).toList());
 		}
 		if (value.hasKvlistValue()) {
-			return "KvList(%s)".formatted(Map.ofEntries(value.getKvlistValue()
+			return "%s".formatted(Map.ofEntries(value.getKvlistValue()
 				.getValuesList()
 				.stream()
 				.map(kv -> Map.entry(kv.getKey(), any(kv.getValue())))
