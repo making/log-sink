bcbb9107626ba7598936b86048f2e6103d2cb92a
Toshiaki Maki <makingx@gmail.com>
2025-05-20 23:38:25 +0900

    Bump Spring Boot from 3.3.4 to 3.4.5

--- src/test/java/lol/maki/logsink/IntegrationTestBase.java
+++ src/test/java/lol/maki/logsink/IntegrationTestBase.java
@@ -1,16 +1,12 @@
 package lol.maki.logsink;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.BasicJsonTester;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestClient;
+import org.zalando.logbook.spring.LogbookClientHttpRequestInterceptor;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 		properties = { "logging.logback.ecs-encoder.enabled=false" })
@@ -33,11 +29,7 @@ public abstract class IntegrationTestBase {
 	public void init() {
 		if (this.restClient == null) {
 			this.restClient = this.restClientBuilder.baseUrl("http://localhost:" + port)
-				.defaultStatusHandler(new DefaultResponseErrorHandler() {
-					@Override
-					protected void handleError(ClientHttpResponse response, HttpStatusCode statusCode) {
-
-					}
+				.defaultStatusHandler(__ -> true, (req, res) -> {
 				})
 				.requestInterceptor(this.logbookClientHttpRequestInterceptor)
 				.build();
